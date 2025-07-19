50e37184b4d141b2d60def3649c8f18b855d256f
Samyakmanwatkar10 <samyakmanwatkar@gmail.com>
2025-07-19 20:55:18 +0530

    added prediction

--- main.py
+++ main.py
@@ -23,7 +23,19 @@ X=scaler.fit_transform(X)
 
 # train test split
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,stratify=Y,random_state=2)
-print(X.shape, X_train.shape, X_test.shape)
+# print(X.shape, X_train.shape, X_test.shape)
 
 # Training the model
+classifier=svm.SVC(kernel='linear')
+classifier.fit(X_train, Y_train)
 
+# Model Evaluation
+# accuracy score on training data
+X_train_prediction=classifier.predict(X_train)
+training_data_accuracy=accuracy_score(X_train_prediction, Y_train)
+print('Accuracy on training data: ', training_data_accuracy)
+
+# accuracy score on test data
+X_test_prediction=classifier.predict(X_test)
+test_data_accuracy=accuracy_score(X_test_prediction, Y_test)
+print("Accuracy on test data: ", test_data_accuracy)
